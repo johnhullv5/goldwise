4fe5e9d79fda4ea64c423ed8112cce20d0370e30
johnhullv5 <jhihll@163.com>
2016-05-06 14:48:44 -0400

    helper function add get exchanges and symbols path.

--- helper.py
+++ helper.py
@@ -1,3 +1,36 @@
+import configparser
+
+def get_exchanges():
+	Config = configparser.ConfigParser()
+
+	Config.read("goldenwise.ini")
+
+
+	exchanges = []
+
+
+	for (each_key, each_val) in Config.items("Exchanges"):
+        	if each_val not in exchanges:
+                	exchanges.append(each_val)
+	return exchanges
+
+def get_symbols_path():
+	symbols_path = ""
+
+	Config = configparser.ConfigParser()
+
+        Config.read("goldenwise.ini")
+
+	for (each_key, each_val) in Config.items("Path"):
+        	if each_key=="symbols":
+                	symbols_path=each_val
+
+	return symbols_path
+
+
+
+
+
 def ConfigSectionMap(section):
     dict1 = {}
     options = Config.options(section)
