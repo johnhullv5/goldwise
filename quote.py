72a588e8df06f0f1f343eae59f206e07c2a723c5
johnhullv5 <jhihll@163.com>
2016-05-09 08:59:16 -0400

    work download

--- quote.py
+++ quote.py
@@ -15,29 +15,35 @@ class quote_downloader():
 		self.mode = mode
 		self.start = start
 		self.end = end
-
-		self.symbols =  load_only_symbols()
+		break_symbol = read_break_downloading_symbol()
+		self.symbols =  make_download_list(break_symbol)
 		#self.symbols = self.symbols[:10]
 		self.datasource = 'yahoo'
 		#print(self.symbols)
 		self.failed_symbols = []
 
 		self.quote_path = get_quote_path()
+		self.current_download_symbol = None
 
 	def parallel_down(self):
 		try:
 			Parallel(n_jobs=2)(delayed(self.get_quote)(symbol) for symbol in self.symbols)
 		except:
+			write_break_downloading_symbol(self.current_download_symbol)
 			logging.critical("parallel process error")
+			sys.exit(1)
+			
+		
 	
 
 	def get_quote(self,symbol):
 
 		try:
-			time.sleep(0.05)	
+			time.sleep(0.01)	
 			df = web.DataReader(symbol, self.datasource, self.start, self.end)
 			df.to_csv(self.quote_path+symbol+".csv")
 			print(symbol+" process done!")
+			self.current_download_symbol = symbol
 			
 			
 		except:
